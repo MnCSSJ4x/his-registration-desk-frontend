d8e139aa961d2f662ef9405cd0f27ce3a7834bbe
Chinthan23 <77500564+Chinthan23@users.noreply.github.com>
2024-05-05 23:11:26 +0530

    fetch prescriptions done, to-do: display it

--- src/Pages/Home/Pharmacist/PatientSearch.tsx
+++ src/Pages/Home/Pharmacist/PatientSearch.tsx
@@ -1,51 +1,67 @@
-import React,{useState} from 'react'
+import React,{useEffect, useState} from 'react'
 import Title from '../Components/Title'
 import SearchBar from '../Desk/EditPatient/Components/SearchPatient'
 import { Patient } from '../../Types/Patient'
+import { useRecoilValue } from 'recoil'
+import { authState } from '../../../auth/auth'
+import axios from 'axios'
+
+interface FileImageMap {
+    [key: string]: string;
+}
 
 const PatientSearch = () => {
-  const [patientSelected,setPatient]=useState<Patient|undefined>(undefined);
+  const token=useRecoilValue(authState)
   const [searchTerm, setSearchTerm] = useState<string>('');
-  const [patients, setPatients] = useState<Patient[]>([]);
 	const [fetchedPrescriptions, setFetchedPrescriptions] = useState<any[]>([]);
 	const handleBack = () => {
     setSearchTerm('');
-    setPatient(undefined);
     setFetchedPrescriptions([]); // Clear fetched prescriptions
   };
-	const handleClick = async () => {
-    if (!patientSelected) {
-      alert('Please select a patient first.');
-      return;
-    }
 
-    try {
-      // const response = await fetch(/* URL to fetch prescriptions */);
-      // const fetchedData = await response.json();
-      // setFetchedPrescriptions(fetchedData); // Update prescriptions state
-    } catch (error) {
-      console.error('Error fetching prescriptions:', error);
-      alert('An error occurred while fetching prescriptions.');
-    }
+  const fetchPrescriptions = async () => {
+    console.log(`${process.env.REACT_APP_DB2_URL}/emr/getPrescriptionByEmrIdText/${searchTerm}`,token)
+      const response = await axios.get(`${process.env.REACT_APP_DB2_URL}/emr/getPrescriptionByEmrIdText/${searchTerm}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      console.log(response.data)
+      if(response.status===200){
+        if(Object.keys(response.data).length === 0 && response.data.constructor === Object){
+          setFetchedPrescriptions(["No Prescription!"]);
+        }
+        // setPatients(response.data);
+      }
+      else{
+        alert("Error fetching patients!")
+      }
   };
+
   return (
     <div className="flex flex-col justify-center items-center h-auto mt-20">
       <Title title='View Prescriptions'></Title>
-      <div className="flex flex-row">
-        <SearchBar patients={patients} setPatients={setPatients} onSelect={(patient)=>setPatient(patient)} searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-        <button className="flex flex-row justify-self-start my-10 mx-4 bg-interactive01 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:outline-none" onClick={()=>""}>Get</button>
+      <div className="flex flex-row mt-8">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="w-full bg-gray-200 text-gray-800 rounded-full px-4 h-10 focus:outline-none focus:bg-gray"
+      />
+        <button className="justify-self-start bg-interactive01 text-white ml-2 py-2 px-4 rounded-lg hover:bg-blue-700 focus:outline-none" onClick={()=>fetchPrescriptions()}>Get</button>
       </div>
-			{patientSelected  && (
-        <div className="flex flex-col justify-center">
-          <div className="flex flex-col items-center text-text01 text-2xl px-4 font-bold">Prescriptions for {patientSelected.name}</div>
-          Fetched Prescriptions....
-					{/* Iterate over fetchedPrescriptions and render prescription details */}
-          {fetchedPrescriptions.map((prescription) => (
-            <div key={prescription.id}>
-              {/* Display prescription details (medication, dosage, etc.) */}
-							Fetched Prescription....
-            </div>
-          ))}
+			{fetchedPrescriptions.length>=1  && (
+        <div className="flex flex-col justify-center mt-8">
+          <div className="flex flex-col items-center text-text01 text-2xl px-4 font-bold">Prescriptions for {searchTerm}</div>
+          Fetched Prescriptions...
+          {fetchedPrescriptions.length===1 && <div> {fetchedPrescriptions[0]}</div>}
+          {Object.entries(fetchedPrescriptions).map(([key, value]) => (
+                <div key={key}>
+                    <p>{key}</p>
+                    <img src={`data:image/jpeg;base64, ${value}`} alt={key} />
+                </div>
+            ))}
 					<button className="my-10 mx-4 bg-interactive01 text-white py-2 px-4 rounded-lg hover:bg-blue-700 focus:outline-none" onClick={handleBack}>
 						Back
 					</button>
